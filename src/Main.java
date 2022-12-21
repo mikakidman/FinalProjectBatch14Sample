3bdd04f636a8dd88ad7296656a307231bddd8035
mikakidman <kitmanovamz@gmail.com>
2022-12-20 19:28:53 -0500

    the basic solar system with the feature sun added is ready

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem Xron=new SolarSystem();
+        Xron.moon=2;
+        Xron.sun=1;
+        Xron.planets=3;
+
+        FeatureSun Fxron=new FeatureSun();
+        Fxron.color="green";
+        Fxron.size="12345km";
+        Fxron.temp="256";
+    }
+}
